9ddb23f4e501b2728362ffa1959ce134fea0a6da
matix0508 <matix0508@gmail.com>
2020-10-16 02:22:38 +0200

    adding functionality to solve matrix equations of type Ax=b

--- matrix.py
+++ matrix.py
@@ -1,5 +1,11 @@
 from errors import WrongDimensionsError
 
+def epsilon(i, j):
+    if (i, j) == (0, 1):
+        return 1
+    if (i, j) == (1, 0):
+        return -1
+
 class Matrix:
     def __init__(self, rows, cols):
         self.rows = rows
@@ -44,7 +50,7 @@ class Matrix:
         if self.cols > 1:
             output += "]"
 
-        return output
+        return output + "\n"
 
     def same_type(self, other):
         if self.rows == other.rows and self.cols == other.cols:
@@ -95,3 +101,62 @@ class Matrix:
             for j in range(self.rows):
                 output.table[i][j] = self.table[j][i]
         return output
+
+    def det(self):
+        total=0
+        # Section 1: store indices in list for row referencing
+        indices = list(range(len(self.table)))
+
+        # Section 2: when at 2x2 submatrices recursive calls end
+        if len(self.table) == 2 and len(self.table[0]) == 2:
+            val = self.table[0][0] * self.table[1][1] - self.table[1][0] * self.table[0][1]
+            return val
+
+        # Section 3: define submatrix for focus column and
+        #      call this function
+        for fc in indices: # A) for each focus column, ...
+            # find the submatrix ...
+            As = Matrix(self.rows, self.cols)
+            As.table = self.table
+                 # B) make a copy, and ...
+            As.table = As.table[1:] # ... C) remove the first row
+            height = len(As.table) # D)
+
+            for i in range(height):
+                # E) for each remaining row of submatrix ...
+                #     remove the focus column elements
+                As.table[i] = As.table[i][0:fc] + As.table[i][fc+1:]
+
+            sign = (-1) ** (fc % 2) # F)
+            # G) pass submatrix recursively
+            sub_det = As.det()
+            # H) total all returns from recursion
+            total += sign * self.table[0][fc] * sub_det
+
+        return total
+
+    def change_col(self, colidx, col):
+        output = Matrix(self.rows, self.cols)
+        output.get_data(self.table)
+        for i in range(self.rows):
+            output.table[i][colidx] = col[i][0]
+        return output
+
+
+
+
+
+
+def solve(A, b):
+    rows = b.rows
+    W = A.det()
+    x = []
+    print(W)
+    for i in range(rows):
+        Wn = A.change_col(i, b.table)
+        print(Wn)
+        x.append([Wn.det()/W])
+    output = Matrix(rows, 1)
+    print(x)
+    output.get_data(x)
+    return output
